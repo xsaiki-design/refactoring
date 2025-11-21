ca86c3c389472a5f8ffa763dca5448ca1a27b98f
xande <xandersaiki@gmail.com>
2025-11-21 16:50:12 -0500

    Testing I can Push

--- src/main/java/theater/Constants.java
+++ src/main/java/theater/Constants.java
@@ -4,13 +4,14 @@ package theater;
  * Constants used in this program.
  */
 public final class Constants {
+    // Testing some things
 
     // volume constants
     public static final int BASE_VOLUME_CREDIT_THRESHOLD = 30;
     public static final int COMEDY_EXTRA_VOLUME_FACTOR = 5;
     // comedy amount constants
     public static final int COMEDY_AMOUNT_PER_AUDIENCE = 300;
-    public final static int COMEDY_AUDIENCE_THRESHOLD = 20;
+    public static final int COMEDY_AUDIENCE_THRESHOLD = 20;
     public static final int COMEDY_BASE_AMOUNT = 30000;
     public static final int COMEDY_OVER_BASE_CAPACITY_AMOUNT = 10000;
     public static final int COMEDY_OVER_BASE_CAPACITY_PER_PERSON = 500;
